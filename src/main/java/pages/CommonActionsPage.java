866b75c0c8a96519a17bc9789b710761433e2c1b
AlfredoMc95 <alfredomartcam@gmail.com>
2025-02-25 09:09:15 -0500

    first commit

---
+++ src/main/java/pages/CommonActionsPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class CommonActionsPage {
+
+    protected WebDriver driver;
+
+    public CommonActionsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebDriver firefoxDriverConnection() {
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    public void visitPage(String url) {
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    public void cleanInputText(By localization) {
+        driver.findElement(localization).clear();
+    }
+
+    public void typeInputText(By localization, String text) {
+        driver.findElement(localization).sendKeys(text);
+    }
+
+    public void sendText(By localization){
+        driver.findElement(localization).submit();
+    }
+
+    public void implicitWait(int seconds) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
+    }
+
+    public void getTitle() {
+        driver.getTitle();
+    }
+}
